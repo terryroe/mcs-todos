43a29adc56fe59f7cfea0784dcd8e35a38058e5c
Terry Roe <terryroe@gmail.com>
2023-09-30 11:51:13 -0600

    fetch and set todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { API_URL } from './data/api';
 
 function App() {
-  return <div></div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function getTodos() {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setTodos(data);
+    }
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      {todos.map((todo) => (
+        <div>{todo.title}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
